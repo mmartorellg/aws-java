dacb0e7b758fa086c9d75a68b9f4c50e0f718eff
Manuel Martorell Gonzalez <mmartorell@soaint.com>
2025-09-28 09:27:42 +0200

    get

--- src/main/java/com/example/awsjava/controller/DummyController.java
+++ src/main/java/com/example/awsjava/controller/DummyController.java
@@ -1,5 +1,6 @@
 package com.example.awsjava.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1")
 public class DummyController {
 
-    @RequestMapping("/hello")
+    @GetMapping("/hello")
     public String dummy() {
         return "Hello World!";
     }
