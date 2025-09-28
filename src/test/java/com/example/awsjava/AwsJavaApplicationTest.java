4b119b9821c26650327ca841f795ed51a2455c37
Manuel Martorell Gonzalez <mmartorell@soaint.com>
2025-09-29 00:27:56 +0200

    add test

---
+++ src/test/java/com/example/awsjava/AwsJavaApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.awsjava;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AwsJavaApplicationTest {
+
+    @Test
+    void main() {
+        AwsJavaApplication.main(new String[]{});
+    }
+}
