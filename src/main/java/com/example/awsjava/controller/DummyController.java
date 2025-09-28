c03021f5bb1426bea99aa8360074424b7ffd7c0e
Manuel Martorell Gonzalez <mmartorell@soaint.com>
2025-09-29 01:17:54 +0200

    add extra resources

--- src/main/java/com/example/awsjava/controller/DummyController.java
+++ src/main/java/com/example/awsjava/controller/DummyController.java
@@ -1,6 +1,7 @@
 package com.example.awsjava.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +12,15 @@ public class DummyController {
     @GetMapping("/hello")
     public String dummy() {
         return "Hello World!";
+    }
+
+    @PostMapping("/post")
+    public String dummyPost() {
+        return "Hello World from Post!";
+    }
+
+    @GetMapping("/hello/{name}" )
+    public String dummyWithPathVariable(String name) {
+        return "Hello " + name;
     }
 }
